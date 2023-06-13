4dcc98b5bc4d0a125d06dcaf8158fadd9cd43d5d
PavankumarMula <pavanpappy7@gmail.com>
2023-06-13 12:43:33 +0530

    Added User Model

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const showBooksRouter = require("./routes/getBooks");
 const sequelize = require("./util/database");
+const Product=require('./model/productModel');
+const User=require('./model/userModel');
 
 app.use(cors());
 
@@ -12,8 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(showBooksRouter);
 
+Product.belongsTo(User,{constraints:true,onDelete:"CASCADE"});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({force:true})
   .then((result) => {
     app.listen(4000, () => {
       console.log("listening on port 4000...");
